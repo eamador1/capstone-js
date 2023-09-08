5b6f44e24d7ac40f918940a8b7a4db3d0cb6462f
Donice <technocrater.pdevelopers@gmail.com>
2023-09-08 15:51:58 +0100

    Fixed linters

--- src/modules/createButton.js
+++
@@ -1,8 +0,0 @@
-const createButton = (text, className) => {
-  const button = document.createElement("button");
-  button.textContent = text;
-  button.className = className;
-  return button;
-};
-
-export default createButton;
