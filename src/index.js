616b2f952bd89ce618f8aa18c337dcf1bd0c3529
gdvl777 <gdvl777@gmail.com>
2023-09-09 10:50:58 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles.css';
-import mylogo from ""./images/chicken_food_logo.png";
+import mylogo from './images/chicken_food_logo.png';
 import getItems from './modules/getItems.js';
 
 document.querySelector('#logo').src = mylogo;
